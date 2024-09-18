335c9be43167df756a3791533219e7f181ec3755
Serj Haben <platinum03@gmail.com>
2024-09-18 19:26:01 +0300

    Initial Commit

---
+++ GitTestTests/GitTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestTests.swift
+//  GitTestTests
+//
+//  Created by Serj on 18.09.2024.
+//
+
+import Testing
+@testable import GitTest
+
+struct GitTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
